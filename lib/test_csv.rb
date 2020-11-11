99955d457d208be46aeeec05ee3881374c531033
annie bartlett <annie-bartlett@outlook.com>
2020-11-11 15:24:19 +0000

    Update test to include context & subject

--- lib/test_csv.rb
+++ lib/test_csv.rb
@@ -5,18 +5,3 @@ class CsvImporter
     CSV.parse(File.read('lib/test.csv'), headers: true, converters: :numeric).map(&:to_h)
   end
 end
-
-# def parse_csv
-#   data = []
-#   CSV.foreach('lib/test.csv') do |row|
-#     csv_data = {
-#       number: row[0],
-#       item: row[1],
-#       price: row[2].to_i,
-#       tax: row[3].to_i,
-#       total: row[4].to_i
-#     }
-#     data << csv_data
-#   end
-#   data
-# end
--- spec/test_csv_spec.rb
+++ spec/test_csv_spec.rb
@@ -2,28 +2,19 @@ require './lib/test_csv'
 require 'rspec'
 
 describe CsvImporter do
-  let(:data) { CsvImporter.parse_csv }
-  let(:id) { data.map { |row| row.values[0].include? "_" } }
-  let(:title) { data.map { |row| row.values[1] =~ /^[A-Z][a-z]+$/ } }
-  let(:price) { data.map { |row| row.values[2] } }
-  let(:tax) { data.map { |row| row.values[3] } }
-  let(:total) { data.map { |row| row.values[4] } }
+  context 'test row' do
+    subject { CsvImporter.parse_csv[3] }
 
-  describe '#ID number' do
-    it 'does not contain underscore' do
-      expect(id).not_to include(true)
-    end
-  end
-
-  describe '#Total' do
-    it 'provides the correct total' do
-      expect([price, tax].transpose.map { |sum| sum.reduce(:+) }).to match(total)
-    end
-  end
-
-  describe '#Product title' do
     it 'follows the correct format' do
-      expect(title).not_to include(nil)
+      expect(subject).to eq(
+        {
+          'number' => 'csv-4',
+          'item' => 'Blouse',
+          'price' => 20,
+          'tax' => 4,
+          'total' => 24
+        }
+      )
     end
   end
 end
